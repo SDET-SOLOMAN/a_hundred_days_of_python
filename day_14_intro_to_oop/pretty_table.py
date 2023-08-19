c585420ee65e211c458614e8bb86a7280cbf84d9
SQA-SOLOMAN <kavkazus@hotmail.com>
2023-08-19 09:18:38 -0400

    day 14 coffee machine using OOP

---
+++ day_14_intro_to_oop/pretty_table.py
@@ -0,0 +1,6 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column('Pokemon Type',['Pikachu', 'Baltazaar', 'Dragon'])
+table.add_column('Pokemon Superpower',['Fire', 'Thunder', 'Flame Tower'])
+print(table)
