680b924ef56b33f3baa698e4814b80db77811350
SDET-SOLOMAN <sdetsoloman@gmail.com>
2023-09-04 15:57:46 -0400

    Day 28, Turkish - English Flash Cards Game

--- practice.py
+++ practice.py
@@ -3,18 +3,6 @@
 # получите сумму всех чисел,
 # распечатайте все строки, где есть буква 'a'
 
-list_one = ['Hi', 'ananas', 2, None, 75, 'pizza', 36, 100]
-
-# the way I solve it
-
-print([x for x in list_one if type(x) == str and 'a' in x.lower()])
-print(list(filter(lambda x: type(x) == str and 'a' in x.lower(), list_one)))
-
-# the way I would solve in interview :)
-
-new_list = []
-
-for char in list_one:
-    if isinstance(char, str) and 'a' in char.lower():
-        new_list.append(char)
-print(new_list)
+d = {4:'3', 5:4}
+d.pop(4)
+print(d)
